d19e5c16c6c27db752bc8ee4afd0c1b145c329b0
utbowhunter <utbowhunter@yahoo.com>
2015-08-28 08:44:03 +0800

    Commit the newer version

---
+++ app/src/main/java/com/perkdev/lesson_manager/PaymentActivity.java
@@ -0,0 +1,72 @@
+package com.perkdev.lesson_manager;
+
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class PaymentActivity extends ActionBarActivity {
+
+    DBAdapter _myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_layout);
+
+        OpenDb();
+        /// Popuate the List View from the database
+
+
+
+    }
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        CloseDb();
+    }
+    private void OpenDb()
+    {
+        _myDb = new DBAdapter(this);
+        _myDb.open();
+    }
+    private void CloseDb()
+    {
+         _myDb.close();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
